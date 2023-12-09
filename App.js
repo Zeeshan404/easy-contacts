09339e4b67217fdfd1989b33ffecdbf8f85d279d
Zeeshan Abbasi <za@weiswise.com>
2023-12-09 18:57:19 +0500

    urdu fonts installed

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  NotoNastaliqUrdu_400Regular,
+  NotoNastaliqUrdu_500Medium,
+  NotoNastaliqUrdu_600SemiBold,
+  NotoNastaliqUrdu_700Bold,
+} from '@expo-google-fonts/noto-nastaliq-urdu';
+
+const fonts = {
+  'R': NotoNastaliqUrdu_400Regular,
+  'M': NotoNastaliqUrdu_500Medium,
+  'B2': NotoNastaliqUrdu_600SemiBold,
+  'B1': NotoNastaliqUrdu_700Bold,
+}
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={{flex:1, backgroundColor:'tan'}}>
 
+  const [fontLoaded, fontError] = useFonts({
+    ...fonts
+  })
+
+  if (!fontLoaded) {
+    return (
+      <View style={{ ...StyleSheet.absoluteFillObject, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator
+          color="black"
+          size="large"
+        />
+      </View>
+    )
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{
+        flex: 1, backgroundColor: 'tan',
+        justifyContent: 'center',
+        alignItemsL: 'center',
+      }}>
+
+        <Text
+          style={{
+            fontSize: 40,
+            fontFamily: 'B2'
+          }}
+        >
+          asdasdasdasd
+          لکھن
+
+        </Text>
+
+      </View>
     </GestureHandlerRootView>
   );
 }
---
+++ extras.md
@@ -0,0 +1,16 @@
+  
+### Note: The expo-font config plugin is only available for SDK 50 and above. If you are using an older SDK, you can load the font at runtime instead. 
+
+    "plugins": [
+      [
+        "expo-font",
+        {
+          "fonts": [
+            "node_modules/@expo-google-fonts/noto-nastaliq-urdu/NotoNastaliqUrdu_400Regular.ttf",
+            "node_modules/@expo-google-fonts/noto-nastaliq-urdu/NotoNastaliqUrdu_500Medium.ttf",
+            "node_modules/@expo-google-fonts/noto-nastaliq-urdu/NotoNastaliqUrdu_600SemiBold.ttf",
+            "node_modules/@expo-google-fonts/noto-nastaliq-urdu/NotoNastaliqUrdu_700Bold.ttf"
+          ]
+        }
+      ]
+    ]
